1529d23d3545ca5e033899caf6312d6243db3c63
tricuong10302 <tricuong10302@gmail.com>
2024-10-10 15:50:44 +0700

    Merge code

--- BLL/Controllers/Category/ShiftController.cs
+++ BLL/Controllers/Category/ShiftController.cs
@@ -123,4 +123,19 @@ public class ShiftController(IShiftRepository repository) : BaseController<Shift
             return HandleException(ex);
         }
     }
+    
+    [HttpPost("get-all-for-combobox")]
+    public IActionResult GetAllForCombobox(GetAllRequest request)
+    {
+        try
+        {
+            if (!ModelState.IsValid) throw new Exception(CommonFunc.GetModelStateAPI(ModelState));
+            var result = repository.GetAllForCombobox();
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
