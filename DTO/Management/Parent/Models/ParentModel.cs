8d065b7340484ac3758d15168ace11d14d10d8f3
machngoc3600@gmail.com <machngoc3600@gmail.com>
2024-10-13 22:02:31 +0700

    parent thêm tỉnh huyện xã mà k biết có hoạt động hay k

--- DTO/Management/Parent/Models/ParentModel.cs
+++ DTO/Management/Parent/Models/ParentModel.cs
@@ -9,5 +9,12 @@ namespace DTO.Management.Parent.Models
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? Note { get; set; }
+        public string? Province { get; set; }
+        public Guid ProvinceId { get; set; }
+        public string? District { get; set; }
+        public Guid DistrictId { get; set; }
+        public string? Ward { get; set; }
+        public Guid WardId { get; set; }
+
     }
 }
--- GUI/src/app/services/category/province.service.ts
+++ GUI/src/app/services/category/province.service.ts
@@ -4,6 +4,8 @@ import {environment} from "../../../environments/environment";
 import {GetListResponseModel} from "../../models/base/get-list-response.model";
 import {GetListRequestModel} from "../../models/base/get-list-request.model";
 import { ProvinceModel } from '../../models/category/province.model';
+import { SelectListItem } from "../../models/base/select-list-item.model";
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +20,8 @@ export class ProvinceService {
 
   getData(id: string) {
     return this.http.get<ProvinceModel>(this.baseUrl + `province/${id}`);
+  }
+  getCombobox(model: any) {
+    return this.http.post<SelectListItem[]>(this.baseUrl + "province/get-all-for-combobox", model);
   }
 }
